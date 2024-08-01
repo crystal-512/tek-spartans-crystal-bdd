44dc084ab857f490e14313ab08ee92575efb9564
crystal512 <crystal.a512@gmail.com>
2024-07-31 21:52:41 -0400

    Security Tests 2

--- src/test/java/tek/bdd/steps/SecurityTestSteps.java
+++ src/test/java/tek/bdd/steps/SecurityTestSteps.java
@@ -4,33 +4,42 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import tek.bdd.pages.HomePage;
+import tek.bdd.pages.SignInPage;
 import tek.bdd.utility.SeleniumUtility;
 
 public class SecurityTestSteps extends SeleniumUtility {
 
+
     @When("user click on sign in link")
     public void userClickOnSignInLink () {
-        clickOnElement(By.id("signinLink"));
+        clickOnElement(HomePage.SING_IN_LINK);
     }
 
     @Then("validate user is in sign in page")
     public void validateUserIsInSignInPage () {
-        String pageSubTitle = getElementText(By.className("login__subtitle"));
+        String pageSubTitle = getElementText(SignInPage.PAGE_SUBTITLE);
         Assert.assertEquals("Sign In", pageSubTitle);
 
     }
 
     @When("user enter username and password and click login")
     public void userEnterUsernameAndPasswordAndClickLogin () {
-        sendText(By.name("email"), "crystal.a512@gmail.com");
-        sendText(By.name("password"), "CC512cc!");
-        clickOnElement(By.id("loginBtn"));
+        sendText(SignInPage.EMAIL_INPUT, "crystal.a512@gmail.com");
+        sendText(SignInPage.PASSWORD_INPUT, "CC512cc!");
+        clickOnElement(SignInPage.LOGIN_BUTTON);
 
     }
 
+    @When("user enter {string} and {string} and click on login")
+    public void userEnterUsernameAndPassword(String username, String password) {
+        sendText(SignInPage.EMAIL_INPUT, username);
+        sendText(SignInPage.PASSWORD_INPUT, password);
+        clickOnElement(SignInPage.LOGIN_BUTTON);
+    }
     @Then("user should be able to see account link")
-    public void userShouldSeeAccountLink () {
-        boolean isAccountDisplayed = isElementDisplayed(By.id("accountLink"));
+    public void userShouldBeAbleToSeeAccountLink() {
+        boolean isAccountDisplayed = isElementDisplayed(HomePage.ACCOUNT_LINK);
         Assert.assertTrue(isAccountDisplayed);
 
     }
--- src/test/resources/feature/SecurityTest.feature
+++ src/test/resources/feature/SecurityTest.feature
@@ -3,7 +3,7 @@ Feature:Security tests scenario
   Scenario:validate sign in functionality with valid credential
     When user click on sign in link
     Then validate user is in sign in page
-    #When user enter "crystal.a512@gmail.com" and "CC512cc!" and click login
-    When user enter username and password and click login
+    When user enter "crystal.a512@gmail.com" and "CC512cc!" and click login
+    #When user enter username and password and click login
     Then user should be able to see account link
 
